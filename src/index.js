8de9177cdd5dfddb28c5ff3782eb0b5a98cc7055
张嘉庆 <jiaqing.zhang@chinairap.com>
2019-11-30 23:44:28 +0800

    剩下最后一个需求

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import './index.css';
 function Square (props) {
   return (
     <button
-      className="square"
+      className={"square "+props.className}
       onClick={props.onClick}
     >
       {props.value}
@@ -29,11 +29,21 @@ function Square (props) {
 class Board extends React.Component {
   render() {
     let board = [];
+    const winnerPosition = this.props.winnerPosition;
     for (let i = 0; i < 3; i++) {
       let row = []
       for (let j = 0; j < 3; j++) {
+        let isWin = false
+        if (winnerPosition) {
+          for (let k = 0; k < winnerPosition.length; k++) {
+            if (winnerPosition[k] === (j + (i * 3))) {
+              isWin = true
+              break
+            }
+          }
+        }
         row.push((
-          <Square key={ (j + (i * 3)) }
+          <Square key={ (j + (i * 3)) } className={isWin ? 'win' : ''}
             value={this.props.squares[j + (i * 3)]} 
             onClick={() => { this.props.onClick(j + (i * 3))} }
           />
@@ -63,7 +73,8 @@ class Game extends React.Component {
         index: null
       }],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      orderSorting: true,
     }
   }
 
@@ -72,7 +83,8 @@ class Game extends React.Component {
     const current = history[history.length - 1]
     const squares = current.squares.slice();
 
-    if (calculateWinner(squares) || squares[i]) {
+    const obj = calculateWinner(squares)
+    if ((obj? obj.winner:null) || squares[i]) {
       return;
     }
     // squares[i] = 'X';
@@ -84,7 +96,7 @@ class Game extends React.Component {
         index: i
       }]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
     });
   }
 
@@ -95,20 +107,33 @@ class Game extends React.Component {
     })
   }
 
+  orderHistory () {
+    this.setState({
+      orderSorting: !this.state.orderSorting
+    })
+  }
+
   render() {
     const history = this.state.history
     const current = history[this.state.stepNumber]
-    const winner = calculateWinner(current.squares);
+    const obj = calculateWinner(current.squares);
+    const winner = obj ? obj.winner : null
 
-    const moves = history.map((step, move) => {
+    // 这个循环有疑问。。。。move变量只能定义在for循环条件内。。。
+    //（估计是变量作用域问题）
+    const moves = [];
+    for (let i = 0,move = (this.state.orderSorting ? 0 : (history.length-1));
+      i < history.length;
+      i++,this.state.orderSorting ? move++ : move--) {
+      const step = history[move]
       const desc = move ? 'Go to move #' + move : 'Go to game start'
-      return (
+      moves.push((
         <li key={move}>
           <button className={ move === this.state.stepNumber ? 'currentStep' : '' } onClick={() => this.jumpTo(move)}>{desc}</button>
           <span>{ (step.index !== null ? getColAndRow(step.index) : '') }</span>
         </li>
-      )
-    })
+      ))
+    }
 
     let status;
     if (winner) {
@@ -121,12 +146,14 @@ class Game extends React.Component {
       <div className="game">
         <div className="game-board">
           <Board 
+            winnerPosition={winner ? obj.position : null}
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
           <div>{ status }</div>
+          <button onClick={() => this.orderHistory()}>{this.state.orderSorting ? '倒序' : '正序'}</button>
           <ol>{ moves }</ol>
         </div>
       </div>
@@ -178,10 +205,14 @@ function calculateWinner(squares) {
     [2, 4, 6]
   ];
 
-  for (let i = 0; i < lines.length; i++) {
+  let i
+  for (i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return {
+        winner: squares[a],
+        position: lines[i]
+      };
     }
   }
 
